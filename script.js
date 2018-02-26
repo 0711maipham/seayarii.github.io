c6e4d7fde72b837a39277ae8cfdfff9eb0fdd2d4
Maiphuong Pham <you@ git config --global user.email maipham@ucdavis.edu>
2018-02-25 23:49:59 -0700

    new changes

---
+++ script.js
@@ -0,0 +1,27 @@
+// $(function () {
+//     $("#kuraokami").hover(function (e) {
+//         $(this).html("Tonette Pham").fadeIn();
+//     }, function (e) {
+//         $(this).html("KURAOKAMI");
+//     });
+// });
+
+
+// function scroll() {
+//     var element = document.getElementById("skills");
+//     element.scrollIntoView({behavior:"smooth"});
+//     console.log("success")
+// };
+
+// scroll();
+
+$(document).ready(function(){
+    $('.slideNav').click(function(e){
+        var linkHref= $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(linkHref).offset().top
+        });
+
+        e.preventDefault();
+    })
+})
